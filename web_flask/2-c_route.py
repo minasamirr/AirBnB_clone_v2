a6934048bf63bfa18d0a25add66a14975f53a399
minasamirr <mina430q@gmail.com>
2024-05-04 07:36:04 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -40,7 +40,7 @@ def cisamazing(text):
         str: The message "C " followed by the value of the text variable
              (with underscores replaced by spaces).
     """
-    return 'c' + text.replace('_', '')
+    return 'C' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
