d97206255581d041329b8f4d29535199cc71ca6f
minasamirr <mina430q@gmail.com>
2024-05-04 07:17:03 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Script that starts a Flask web application.
-C is fun!
 """
 
 from flask import Flask, escape
@@ -13,24 +12,37 @@ app = Flask(__name__)
 def hello_hbnb():
     """
     Route that returns a message when accessed.
+
+    Returns:
+        str: A greeting message "Hello HBNB!".
     """
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """
     Route that returns a message when accessed.
+
+    Returns:
+        str: A message "HBNB".
     """
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     """
     Route that returns a message when accessed.
+
+    Args:
+        text (str): The text variable appended to "C".
+
+    Returns:
+        str: The message "C " followed by the value of the text variable
+             (with underscores replaced by spaces).
     """
-    return 'c' + text.replace('_', '')
+    return "C {}".format(escape(text).replace('_', ' '))
 
 
 if __name__ == "__main__":
