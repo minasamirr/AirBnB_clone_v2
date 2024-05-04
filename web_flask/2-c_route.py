84b0020093442c35584645fe267a09a0ca4fe131
minasamirr <mina430q@gmail.com>
2024-05-04 07:18:26 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,54 +1,24 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application.
-
-This script defines routes for a Flask web application to display various messages.
-Routes:
-    /: Displays a greeting message "Hello HBNB!".
-    /hbnb: Displays a message "HBNB".
-    /c/<text>: Displays "C " followed by the value of the text variable (with underscores replaced by spaces).
+starts a Flask web application
 """
-from flask import Flask, escape
 
+from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Route that returns a message when accessed.
-
-    Returns:
-        str: A greeting message "Hello HBNB!".
-    """
-    return "Hello HBNB!"
-
+def index():
+	"""returns Hello HBNB!"""
+	return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """
-    Route that returns a message when accessed.
+def hbnb():
+	"""returns HBNB"""
+	return 'HBNB'
 
-    Returns:
-        str: A message "HBNB".
-    """
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """
-    Route that returns a message when accessed.
-
-    Args:
-        text (str): The text variable appended to "C".
-
-    Returns:
-        str: The message "C " followed by the value of the text variable
-             (with underscores replaced by spaces).
-    """
-    return "C {}".format(escape(text).replace('_', ' '))
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/hbnb', strict_slashes=False)
+def cisamazing(text):
+	"""display "c" folloed by the value of the text variable"""
+	return 'c' + text.replace('_', '')
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port='5000')
